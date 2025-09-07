58b59be8caf39f85954fc8615bce36e556b79156
Amit Sah <aksamitsah@gmail.com>
2025-09-08 02:28:07 +0530

    Initial Commit

---
+++ Zaytun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zaytun
+//
+//  Created by Amit Shah on 08/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
